61d3fba14ee54114432617b50e080c8c8c7518a9
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-06-13 15:01:08 +0800

    commited

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,73 +1,71 @@
-
-
 // blog data
 
 export const Blogs = [
-    {
-        id:1,
-        name:"Netflix Clone Application",
-        tags:["react","sass","node js","mongodb"],
-        date:"13 April, 2022",
-        imgSrc:"CV/neflixclone.jpeg",
-        link:"https://netflix-clone-live.netlify.app/"
-    },
-    {
-        id:2,
-        name:"YouTube Clone Application",
-        tags:["react","sass","mongodb","restAPI"],
-        date:"5 May 2022",
-        imgSrc:"CV/youtubeclone.jpeg",
-        link:"https://my-utube-app-new.web.app/"
-    },
-    {
-        id:3,
-        name:"Video Chat Application",
-        tags:["react","Node Js","material ui"],
-        date:"20 May 2022",
-        imgSrc:"CV/videochatnew.jpeg",
-        link:"https://video-calling-application-site.netlify.app/"
-    },
-    {
-        id:4,
-        name:"Travel-Advisor-Application",
-        tags:["react","material UI","Rest API"],
-        date:"05 Jun 2022",
-        imgSrc:"CV/travelapp.jpeg",
-        link:"https://travel-advisor-application-new.netlify.app/"
-    },
-    {
-        id:5,
-        name:"Hotel Booking Application",
-        tags:["react","fontawesome","nodejs","mongodb"],
-        date:"7 Jun 2022",
-        imgSrc:"CV/bookingapp.jpeg",
-        link:"https://hotel-reservation-application.netlify.app/"
-    },
-    {
-        id:6,
-        name:"Blog Application",
-        tags:["react","material UI","node js","mongodb"],
-        date:"10 Jun, 2021",
-        imgSrc:"CV/blogapp.jpeg",
-        link:"https://myblog-application.netlify.app/"
+  {
+    id: 1,
+    name: "Netflix Clone Application",
+    tags: ["react", "sass", "node js", "mongodb"],
+    date: "13 April, 2022",
+    imgSrc: "CV/neflixclone.jpeg",
+    link: "https://netflix-clone-live.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Hotel Booking Application",
+    tags: ["react", "fontawesome", "nodejs", "mongodb"],
+    date: "7 Jun 2022",
+    imgSrc: "CV/bookingapp.jpeg",
+    link: "https://hotel-reservation-application.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Travel-Advisor-Application",
+    tags: ["react", "material UI", "Rest API"],
+    date: "05 Jun 2022",
+    imgSrc: "CV/travelapp.jpeg",
+    link: "https://travel-advisor-application-new.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "POS Application",
+    tags: ["react", "css", "node js", "mongodb"],
+    date: "30 April, 2021",
+    imgSrc: "CV/posapp.jpeg",
+    link: "https://pos-application.netlify.app/",
+  },
+  {
+    id: 5,
+    name: "Blog Application",
+    tags: ["react", "material UI", "node js", "mongodb"],
+    date: "10 Jun, 2021",
+    imgSrc: "CV/blogapp.jpeg",
+    link: "https://myblog-application.netlify.app/",
+  },
+  {
+    id: 6,
+    name: "YouTube Clone Application",
+    tags: ["react", "sass", "mongodb", "restAPI"],
+    date: "5 May 2022",
+    imgSrc: "CV/youtubeclone.jpeg",
+    link: "https://my-utube-app-new.web.app/",
+  },
+  {
+    id: 7,
+    name: "Video Chat Application",
+    tags: ["react", "Node Js", "material ui"],
+    date: "20 May 2022",
+    imgSrc: "CV/videochatnew.jpeg",
+    link: "https://video-calling-application-site.netlify.app/",
+  },
 
-    },
-   
-    {
-        id:7,
-        name:"POS Application",
-        tags:["react","css","node js","mongodb"],
-        date:"30 April, 2021",
-        imgSrc:"CV/posapp.jpeg",
-        link:"https://pos-application.netlify.app/"
-    },
-    // {
-    //     id:8,
-    //     name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
-    //     tags:["react","framer-motion","styled-components"],
-    //     date:"26 May, 2021",
-    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
-    //     link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
-    // },
   
-] 
+
+  // {
+  //     id:8,
+  //     name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+  //     tags:["react","framer-motion","styled-components"],
+  //     date:"26 May, 2021",
+  //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+  //     link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
+  // },
+];
